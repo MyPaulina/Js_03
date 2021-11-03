41905e170b2d5cc6d6316f2f2bc0be6fd56ce66c
PAULINA SALAZAR <83146423+MyPaulina@users.noreply.github.com>
2021-11-02 22:37:16 -0600

    Add files via upload

---
+++ Js_03/src/Js_03.js
@@ -0,0 +1,24 @@
+
+
+ // -.{{´+¿'1|;:_[Ñ*¨¡?=)(//&%$#"!°|°<    > 
+
+<html>
+<TITLE> EJEMPLO 03 </TITLE> 
+<head>
+
+< script lenguaje = "Javascript">
+   function confirmar (){
+     return confirm ("Esta página es para mayores de 18 años.")}
+
+
+</script>
+
+</head>
+
+<body>
+   <a href = "hrrp://www. https://www.disneyplus.com/es-mx/"
+   onclick ="return confirmar ()"> Enlace a la pagina oficial Disney México
+   </a>
+
+</body>
+</html>
